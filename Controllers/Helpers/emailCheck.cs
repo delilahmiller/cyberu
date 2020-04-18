47f57ff6b54a0b93a33dba25d6a3f4ee8bd04dc2
Luke Miller <lukas.miller@live.com>
2020-04-18 11:15:27 -0500

    Auto stash before merge of "master" and "origin/master"

--- Controllers/Helpers/emailCheck.cs
+++ Controllers/Helpers/emailCheck.cs
@@ -1,10 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using CyberU.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace CyberU.Models
+namespace CyberU.Controllers
 {
     public class EmailCheck
     {
@@ -29,22 +30,5 @@ namespace CyberU.Models
         }
     }
 
-    public class EmailCheckResult
-    {
-        public String Name;
-        public String Title;
-        public String Domain;
-        public String BreachDate; // Format "2019-01-07"
-        public String AddedDate; // Format "2019-01-16T21:46:07Z",
-        public String ModifiedDate; // Format "2019-01-16T21:50:21Z",
-        public int PwnCount;
-        public String Description;
-        public String LogoPath;
-        public List<String> DataClasses;
-        public bool IsVerified;
-        public bool IsFabricated;
-        public bool IsSensitive;
-        public bool IsRetired;
-        public bool IsSpamList;
-    }
+
 }
---
+++ Models/EmailCheckResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CyberU.Models
+{
+    public class EmailCheckResult
+    {
+        public String Name;
+        public String Title;
+        public String Domain;
+        public String BreachDate; // Format "2019-01-07"
+        public String AddedDate; // Format "2019-01-16T21:46:07Z",
+        public String ModifiedDate; // Format "2019-01-16T21:50:21Z",
+        public int PwnCount;
+        public String Description;
+        public String LogoPath;
+        public List<String> DataClasses;
+        public bool IsVerified;
+        public bool IsFabricated;
+        public bool IsSensitive;
+        public bool IsRetired;
+        public bool IsSpamList;
+    }
+}
